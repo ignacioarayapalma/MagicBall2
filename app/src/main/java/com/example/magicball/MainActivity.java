767ab9774be4bd5fb602edddb3bd0a50729710ea
ignacioarayapalma <ignacio.araya1@mail.udp.cl>
2021-05-09 23:53:16 -0400

    nuevas modificaciones

--- app/src/main/java/com/example/magicball/MainActivity.java
+++ app/src/main/java/com/example/magicball/MainActivity.java
@@ -12,39 +12,43 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-private ImageButton ball;
-private TextView respuesta;
-private String  [] respuestaArray={"\"Es cierto\", \"Definitivamente es así\", \"Sin duda\", \"Sí definitivamente\", \"Puede confiar en él\", \"Como yo lo veo, sí\",\n" +
-        "        \"Lo más probable\", \"Perspectiva buena\", \"Sí\", \"Las señales apuntan a sí\", \"Respuesta confusa intente de nuevo\", \"Pregunte de nuevo más tarde\",\n" +
-        "        \"Mejor no decirte ahora\", \"No puedo predecir ahora\", \"Concéntrate y pregunta de nuevo\", \"No cuentes con eso\",\n" +
-        "        \"Mi respuesta es no\", \"Mis fuentes dicen que no\", \"Outlook no es tan bueno\", \"Muy dudoso\""}
+    private ImageButton ball;
+    private TextView respuesta;
+    private String[] respuestaArray = {"Si esperas que alguien ilumine tu camino, hoy es un buen día para empezar a viajar dentro de tu espíritu.",
+            "Si estás deprimido estás viviendo en el pasado / si estás ancioso estás viviendo en el futuro / si estás en paz, estás viviendo en el presente.",
+            "No escales la montaña para que el mundo te pueda ver, sino para que vos puedas ver el mundo.",
+            "Sonrie aunque tu vida se una mierda....a nadie le importa como estas, sino como te ves.",
+            "Especial es quien puede ver en nosotros lo que no mostramos a nadie.",
+            "Callando es como se aprende a oir, escuchando es como se aprende a hablar: después, hablando se aprende a callar.",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         //referencia la imagen
         ball = findViewById(R.id.btn);
         //refencia texto
-        respuesta = findViewById(R.id.respuesta);
+        respuesta = findViewById(R.id.respuestavista);
 
         //proceso
 
         ball.setOnClickListener(this);
-        Toast.makeText(MainActivity.this, "Consulta tu suerte", Toast.LENGTH_SHORT).show();
+        Toast.makeText(MainActivity.this, "Revela tu destino", Toast.LENGTH_SHORT).show();
 
     }
+
     @Override
-    public void onClick(View view){
-    //creación metodo switch
-      switch (v.getID()){
-          case R.id.btn:
+    public void onClick(View v) {
+        //creación metodo switch
+        switch (v.getId()) {
+            case R.id.btn:
 
-              int rand = new Random().nextInt(respuestaArray.length);
-              respuesta.setText(respuestaArray[rand]);
-              break;
-      }
+                int rand = new Random().nextInt(respuestaArray.length);
+                respuesta.setText(respuestaArray[rand]);
+                break;
+        }
     }
-
 }
